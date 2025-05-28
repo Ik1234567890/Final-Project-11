98f04a1707de1a70d46f3695ba32f3ac05e38605
Ik1234567890 <ivchokolev007@gmail.com>
2025-05-28 09:11:56 +0300

    MainAppGUI.java

---
+++ MainAppGUI.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class MainAppGUI {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("StudyBuddy - Flashcard App");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(500, 400);
+            frame.setVisible(true);
+        });
+    }
+}
